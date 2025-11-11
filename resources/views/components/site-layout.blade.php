78273b091b8213b1b674868a8e3d8a17408e57f3
Sito8943 <sito8943@gmail.com>
2025-11-11 15:56:04 +0100

    DESIGN: add a site layout

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+
+<body class="flex flex-col h-screen">
+    <header class="bg-red-200 rounded-3xl px-4 py-2 flex items-center justify-between m-4">
+        <h1 class="text-xl">
+            Sito Laravel
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <a>
+                        Home
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="flex-1 h-full">
+        {{ $slot }}
+    </main>
+    <footer class="bg-red-400 w-full p-8">
+        <div>
+            <p class="text-3xl text-white">
+                Sito Laravel
+            </p>
+        </div>
+    </footer>
+</body>
+
+</html>
