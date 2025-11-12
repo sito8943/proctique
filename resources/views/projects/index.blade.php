be6e7a5015402f07780ac763e3660c1b5629f148
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-12 15:04:05 +0100

    REFACTOR: rendering projects as a grid

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,8 +1,8 @@
 <x-site-layout title='Projects'>
-    <ul>
+    <ul class="grid grid-cols-3 grid-rows-1 gap-4">
         @foreach ($projects as $project)
-            <li>
-                <div class="bg-gray-200 rounded-lg">
+            <li class="h-full">
+                <div class="bg-gray-200 rounded-lg h-full p-4 flex flex-col gap-2">
                     <h2 class="font-bold text-xl">
                         {{ $project->name }}
                     </h2>
