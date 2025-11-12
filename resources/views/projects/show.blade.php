caaa98aaf2d48bf76ebf786f2d788dbc2fb324ff
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-12 15:39:16 +0100

    FEATURE: show view rendering author and description

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,3 +1,13 @@
-<x-site-layout>
-
+<x-site-layout title="{{ $project->name }}">
+    <div class="flex flex-col gap-4 items-start justify-start">
+        <p class="italic">
+            From
+            <a href="/authors/{{ $project->author->id }}" class="transition text-red-400 hover:text-red-300">
+                {{ $project->author->name }}
+            </a>
+        </p>
+        <p>
+            {{ $project->description }}
+        </p>
+    </div>
 </x-site-layout>
