0f9651a33d9f714098aa79c930198fc4228ee70f
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-13 16:39:51 +0100

    FEATURE: Tag Controller CRUD functionality

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,19 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Tag;
 
 class TagController extends Controller
 {
-    function index()
+    public function index()
     {
         $tags = Tag::all();
         return view('tags.index', compact('tags'));
     }
 
-    function show(int $tagId)
+    public function show(int $tagId)
     {
         $tag = Tag::find($tagId);
         return view('tags.show', compact(['tag']));
+    }
+
+    public function create()
+    {
+        return view('tags.create');
+    }
+
+    public function edit($id)
+    {
+        $tag = Tag::find($id);
+        return view('tags.edit', compact(['tag']));
+    }
+
+    public function store(Request $request)
+    {
+        Tag::create([
+            'name' => $request->name
+        ]);
+
+        return $this->index();
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $tag = Tag::find($id);
+
+        $tag->update([
+            'name' => $request->name,
+        ]);
+
+        return $this->index();
+    }
+
+    public function destroy($id)
+    {
+        $tag = Tag::find($id);
+        $tag->delete();
+        return $this->index();
     }
 }
