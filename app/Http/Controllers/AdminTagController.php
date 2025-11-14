018e176f59a2ee2ac7b84e21aa7c52d480f351d0
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-14 15:17:05 +0100

    REFACTOR: Admin tag controller

---
+++ app/Http/Controllers/AdminTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tag;
+
+class AdminTagController extends Controller
+{
+    public function index()
+    {
+        $tags = Tag::all();
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    public function show(int $tagId)
+    {
+        $tag = Tag::find($tagId);
+        return view('admin.tags.show', compact(['tag']));
+    }
+
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'color' => []
+        ]);
+
+        Tag::create($validated);
+
+        return redirect('/admin/tags');
+    }
+
+    public function edit($id)
+    {
+        $tag = Tag::find($id);
+        return view('admin.tags.edit', compact(['tag']));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'color' => []
+        ]);
+
+        $tag = Tag::find($id);
+
+        $tag->update($validated);
+
+        return redirect('/admin/tags');
+    }
+
+    public function destroy($id)
+    {
+        $tag = Tag::find($id);
+        $tag->delete();
+
+        return redirect('/admin/tags');
+    }
+}
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -18,44 +18,4 @@ class TagController extends Controller
         $tag = Tag::find($tagId);
         return view('tags.show', compact(['tag']));
     }
-
-    public function create()
-    {
-        return view('tags.create');
-    }
-
-    public function edit($id)
-    {
-        $tag = Tag::find($id);
-        return view('tags.edit', compact(['tag']));
-    }
-
-    public function store(Request $request)
-    {
-        Tag::create([
-            'name' => $request->name,
-            'color' => $request->color,
-        ]);
-        return $this->index();
-    }
-
-    public function update(Request $request, $id)
-    {
-
-        $tag = Tag::find($id);
-
-        $tag->update([
-            'name' => $request->name,
-            'color' => $request->color,
-        ]);
-
-        return $this->index();
-    }
-
-    public function destroy($id)
-    {
-        $tag = Tag::find($id);
-        $tag->delete();
-        return $this->index();
-    }
 }
