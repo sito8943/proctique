42f31832e36971ebc35cb7daa436b1a44f55c795
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-18 15:28:02 +0100

    FIX: changing form actions to admin on create/edit form of tags

--- resources/views/admin/tags/create.blade.php
+++ resources/views/admin/tags/create.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout title="Create a new tag">
-    <x-form-layout method="POST" action="/tags">
+    <x-form-layout method="POST" action="/admin/tags">
         <x-text-input name="name" id="name" label="Name" :value="old('name', '')"
             placeholder="Ex: Coding tool"></x-text-input>
         <x-color-input name="color" id="color" :value="old('color', '#ccffdd')" />
--- resources/views/admin/tags/edit.blade.php
+++ resources/views/admin/tags/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout title='Edit tag with Id: {{ $tag->id }}'>
-    <x-form-layout method="PUT" action="/tags/{{ $tag->id }}">
+    <x-form-layout method="PUT" action="/admin/tags/{{ $tag->id }}">
         <x-text-input name="name" id="name" label="Name" :value="old('name', $tag->name)"
             placeholder="Ex: Coding tool"></x-text-input>
 
