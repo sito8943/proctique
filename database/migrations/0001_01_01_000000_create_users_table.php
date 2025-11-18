dc5be43e98a9d5bec46bef44d0597bcaaddf4d65
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-18 11:07:17 +0100

    FEATURE: added is_admin prop to User

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('avatar')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
