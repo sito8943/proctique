b4f5d9a3e37dbc118808c9d759a327b079e75019
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-18 15:12:37 +0100

    REFACTOR: Admin Header to use header tag and nav tags

--- resources/views/layouts/app_navigation.blade.php
+++ resources/views/layouts/app_navigation.blade.php
@@ -1,6 +1,6 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<header x-data="{ open: false }" class="bg-white border-b border-gray-100 sticky top-0">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav class="m-auto w-full max-w-screen-xl px-4">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -77,10 +77,10 @@
                 </button>
             </div>
         </div>
-    </div>
+    </nav>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <nav :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-breeze.responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
@@ -103,13 +103,12 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-breeze.responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-breeze.responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-breeze.responsive-nav-link>
                 </form>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</header>
