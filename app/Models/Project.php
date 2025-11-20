3ee4c8b0bb394a19055368b6ff9e7c14b9bfc302
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-20 15:18:04 +0100

    FEATURE: adding media library spatia

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Project extends Model
+class Project extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+    use InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -63,5 +68,12 @@ class Project extends Model
         return [
             'is_published' => 'bool'
         ];
+    }
+
+    public function registerMediaConversion(Media $media)
+    {
+        $this->addMediaConversion('preview')->fit(Fit::Contain, 320, 200)->nonQueued();
+
+        $this->addMediaConversion('website')->fit(Fit::Contain, 640, 400)->nonQueued();
     }
 }
