0ebfc54648a89cb26400ce2a10cba4d0708f6b83
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-24 15:40:35 +0100

    FEATURES: Main structure for welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Project;
 
 class WelcomeController extends Controller
 {
     function __invoke()
     {
-        return view('welcome');
+
+        $recentProjects = Project::query()
+            ->select('id', 'author_id', 'leading', 'published_at', 'name')
+            ->with('author:id,name', 'tags', 'media')
+            ->where('is_published', true)
+            ->latest('published_at')
+            ->take(4)
+            ->get();
+
+        $mostRecentProject = $recentProjects->shift();
+
+        return view('welcome', compact('mostRecentProject', 'recentProjects'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,45 @@
 <x-site-layout title="Proctique">
+    <section id="hero">
+
+    </section id='trending-projects'>
+
+    <section>
+        <h2 class="text-6xl font-semibold my-10">
+            Trending Projects
+        </h2>
+    </section>
+
+    <section id="recent-projects">
+        <h2 class="text-6xl font-semibold my-10">
+            Recent projects
+        </h2>
+
+        <div class="flex gap-10 border border-slate-100 shadow-sm p-4 rounded-lg">
+            <x-media-image :model="$mostRecentProject" conversion="website"
+                class="aspect-video w-full object-cover rounded-lg" />
+            <div class="flex flex-col gap-4">
+                <h3 class="font-bold text-2xl sm:text-3xl lg:text-4xl">
+                    {{ $mostRecentProject->name }}
+                </h3>
+                <x-author :date="$mostRecentProject->published_at" :author="$mostRecentProject->author"></x-author>
+                <x-tags :tags="$mostRecentProject->tags"></x-tags>
+                <p class="text-sm sm:text-base">
+                    {{ $mostRecentProject->leading }}
+                </p>
+                <a>
+                    Read
+                </a>
+            </div>
+        </div>
+
+        <div class="mt-10">
+            <x-project-grid :projects="$recentProjects" />
+        </div>
+    </section>
+
+    <section id="register-cta">
+        <h2 class="text-6xl font-semibold my-10">
+            What are you waiting for?
+        </h2>
+    </section>
 </x-site-layout>
