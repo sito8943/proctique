a623838a2cc5fb70f599af9c1fb04f0c9d610348
Sito <40501794+sito8943@users.noreply.github.com>
2025-11-24 15:58:55 +0100

    REFACTOR: Change sidebar title to 'Our topics'

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside id="page-sidebar"
     class="sticky top-[60px] flex-col gap-4 hidden md:flex w-full md:w-80 min-h-40 border border-gray-100 shadow-sm rounded-lg p-3 md:p-4">
     <h2 class="text-xl md:text-2xl font-bold">
-        Trending topics
+        Our topics
     </h2>
     <x-tags :tags="$tags" :orientation="'vertical'"></x-tags>
 </aside>
