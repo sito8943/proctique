1970c36fa76b3e63f03ce107296a4c90606ff6e5
Sito <40501794+sito8943@users.noreply.github.com>
2026-01-19 09:27:57 +0100

    FEATURE: Schedule for backups!!

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command("backup:run --only-db")
+    ->timezone("Europe/Madrid")
+    ->daily()
+    ->at('03:01');
+
+Schedule::command("backup:run")
+    ->timezone("Europe/Madrid")
+    ->sundays()
+    ->at('03:05');
